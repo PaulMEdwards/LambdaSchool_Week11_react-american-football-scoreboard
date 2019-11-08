6a445554c4d88601c897d30e503ab3e7f041946f
Paul M Edwards <pauledwards@gmail.com>
2019-11-07 20:06:13 -0800

    Updates

---
+++ precache-manifest.a06ae412c7f0501d62f66896d8c3fcb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebeb209edfd4792c470f5ee9ec7f055c",
+    "url": "/LambdaSchool_Week11_react-american-football-scoreboard/index.html"
+  },
+  {
+    "revision": "62adc939d0755a9e73da",
+    "url": "/LambdaSchool_Week11_react-american-football-scoreboard/static/css/main.a1b10edd.chunk.css"
+  },
+  {
+    "revision": "0449e1ee178339061e75",
+    "url": "/LambdaSchool_Week11_react-american-football-scoreboard/static/js/2.28e6ce50.chunk.js"
+  },
+  {
+    "revision": "62adc939d0755a9e73da",
+    "url": "/LambdaSchool_Week11_react-american-football-scoreboard/static/js/main.9173ffbb.chunk.js"
+  },
+  {
+    "revision": "316315695ad71f459b00",
+    "url": "/LambdaSchool_Week11_react-american-football-scoreboard/static/js/runtime~main.7aee4b37.js"
+  },
+  {
+    "revision": "1abd8c1a58598974d8415573764ba76a",
+    "url": "/LambdaSchool_Week11_react-american-football-scoreboard/static/media/Ozone.1abd8c1a.ttf"
+  }
+]);
